e3053eed80f0fdb5a0e5a89de474892e2f024802
Alyssa Montgomery <alyssamontgomery99@gmail.com>
2021-03-22 12:35:10 -0600

    Lesson 5.2 - Keeping Score

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -10,6 +10,9 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
     void Start()
     {
@@ -19,6 +22,7 @@ public class Target : MonoBehaviour
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
 
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     Vector3 RandomForce()
@@ -39,6 +43,8 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        gameManager.UpdateScore(pointValue);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
     }
 
     private void OnTriggerEnter(Collider other)
